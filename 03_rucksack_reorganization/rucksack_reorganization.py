1bc150cc51d03c7c57d42fefe5588755555094f4
tim <tim.leonhard@posteo.de>
2022-12-03 11:06:22 +0100

    replace get_priority() function with dictionary

--- 03_rucksack_reorganization/rucksack_reorganization.py
+++ 03_rucksack_reorganization/rucksack_reorganization.py
@@ -7,6 +7,9 @@ begin = time.time()
 
 ###
 
+ALL_ITEMS = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+PRIORITIES = {item: idx+1 for idx, item  in enumerate(ALL_ITEMS)}
+
 def find_shared_item(rucksack: str) -> str:
 	split_idx = len(rucksack)//2
 	comp1, comp2 = set(rucksack[:split_idx]), set(rucksack[split_idx:])
@@ -16,10 +19,6 @@ def find_badge(group: list) -> str:
 	a,b,c = [set(g) for g in group]
 	return a.intersection(b).intersection(c).pop()
 
-def get_priority(item: str):
-	items = list(string.ascii_lowercase) + list(string.ascii_uppercase)
-	return items.index(item) + 1
-
 
 with open("input.txt") as file:
 	rucksacks = [line.strip() for line in file.readlines()]
@@ -28,8 +27,8 @@ shared_items = [find_shared_item(rucksack) for rucksack in rucksacks]
 elve_groups = np.reshape(rucksacks, (-1, 3))
 badges = [find_badge(group) for group in elve_groups]
 
-print(f"Part 1: {sum(get_priority(item) for item in shared_items)}")
-print(f"Part 2: {sum(get_priority(badge) for badge in badges)}")
+print(f"Part 1: {sum(PRIORITIES[item] for item in shared_items)}")
+print(f"Part 2: {sum(PRIORITIES[badge] for badge in badges)}")
 
 ###
 
