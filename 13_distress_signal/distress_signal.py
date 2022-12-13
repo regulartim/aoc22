7b2a85a78cab038cdbc5a00d099f167adf7b264d
tim <tim.leonhard@posteo.de>
2022-12-13 11:05:40 +0100

    replace function with comprehension

--- 13_distress_signal/distress_signal.py
+++ 13_distress_signal/distress_signal.py
@@ -20,16 +20,13 @@ def is_ordered(left, right) -> int:
 	stop_default = -1 if len(left) > len(right) else 1 if len(left) < len(right) else 0
 	return next((result for result in results if result != 0), stop_default)
 
-def count_smaller_packets(packet: list, others: list):
-	return sum(True for other in others if is_ordered(other, packet) == 1)
-
 
 with open("input.txt") as file:
 	packets = [ast.literal_eval(line.strip()) for line in file if line.strip()]
-	pairs = [(packets[i], packets[i+1]) for i in range(0, len(packets), 2)]
 
-packets += DIVIDER_PACKETS
-divider_indices = [1 + count_smaller_packets(div, packets) for div in DIVIDER_PACKETS]
+pairs = [(packets[i], packets[i+1]) for i in range(0, len(packets), 2)]
+divider_indices = [sum(is_ordered(other, divider) == 1 for other in packets + DIVIDER_PACKETS) + 1
+					for divider in DIVIDER_PACKETS]
 
 print(f"Part 1: {sum(idx+1 for idx, pair in enumerate(pairs) if is_ordered(*pair) == 1)}")
 print(f"Part 2: {divider_indices[0] * divider_indices[1]}")
