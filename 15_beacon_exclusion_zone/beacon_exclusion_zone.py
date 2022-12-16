0dbf1f4279e7035683baf871f0afc0de4246ddf7
tim <tim.leonhard@posteo.de>
2022-12-16 12:22:37 +0100

    refactor

--- 15_beacon_exclusion_zone/beacon_exclusion_zone.py
+++ 15_beacon_exclusion_zone/beacon_exclusion_zone.py
@@ -10,46 +10,45 @@ P1_Y_IDX = 2_000_000
 def manhattan_dist(xa :int, ya: int, xb: int, yb: int) -> int:
 	return abs(xa - xb) + abs (ya - yb)
 
-def get_row_coverage(sensor: list, distance: int, row: int) -> tuple:
+def get_row_coverage(sensor: tuple, distance: int, row: int) -> tuple:
 	x, y = sensor
 	radius_at_row = distance - abs(row - y)
 	if radius_at_row < 0:
 		return None
 	return x - radius_at_row, x + radius_at_row
 
-def count_covered_postions(row_coverages: list) -> int:
-	current = row_coverages[0][0]
-	acc = 0
-	gaps = []
-	for start, stop in row_coverages:
-		if current < start:
-			gaps.append(current)
-		if current > stop:
-			continue
-		acc += 1 + stop - max(start, current)
-		current = stop + 1
-	return acc, gaps
-
-def get_row_properties(report: list, distances: list) -> list:
-	result = []
-	for y in range(P1_Y_IDX*2):
-		row_coverages = [get_row_coverage(line[:2], distance, y) for line, distance in zip(report, distances)]
-		row_coverages = sorted([c for c in row_coverages if c])
-		covered_positions, gaps = count_covered_postions(row_coverages)
-		beacons_in_row = set(line[2] for line in report if line[3] == y)
-		result.append((covered_positions - len(beacons_in_row), gaps))
+def merge_intervals(intervals: list) -> tuple:
+	intervals.sort()
+	result = intervals[0]
+	for start, stop in intervals[1:]:
+		if result[1] + 1 < start:
+			break
+		result = result[0], max(result[1], stop)
 	return result
+
+def get_coverage_interval(report: list, distances: list, y: int) -> tuple:
+	row_coverages = [get_row_coverage(line[:2], dist, y) for line, dist in zip(report, distances)]
+	return merge_intervals([i for i in row_coverages if i])
+
+def find_interval_gap(report: list, distances: list) -> tuple:
+	for y in range(P1_Y_IDX * 2):
+		coverage_interval = get_coverage_interval(report, distances, y)
+		if coverage_interval[1] < P1_Y_IDX * 2:
+			return coverage_interval[1] + 1, y
 
 
 with open("input.txt") as file:
-	sensors_report = [[int(n) for n in re.findall(r"-?\d+", line)] for line in file]
-
+	sensors_report = [tuple(int(n) for n in re.findall(r"-?\d+", line)) for line in file]
 beacon_distances = [manhattan_dist(*line) for line in sensors_report]
-row_properties = get_row_properties(sensors_report, beacon_distances)
-distress_x, distress_y = next((row[1][0], y) for y, row in enumerate(row_properties) if row[1])
 
-print(f"Part 1: {row_properties[P1_Y_IDX][0]}")
-print(f"Part 2: {distress_x * 4000000 + distress_y}")
+coverage_start, coverage_stop = get_coverage_interval(sensors_report, beacon_distances, P1_Y_IDX)
+beacons_in_row = set(line[2] for line in sensors_report if line[3] == P1_Y_IDX)
+blocked_postions = abs(coverage_start - coverage_stop) + 1 - len(beacons_in_row)
+
+gap_x, gap_y = find_interval_gap(sensors_report, beacon_distances)
+
+print(f"Part 1: {blocked_postions}")
+print(f"Part 2: {gap_x * 4000000 + gap_y}")
 
 ###
 
