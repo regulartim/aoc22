825791c66425a83c014344ef1874fd9e39e8c6e9
tim <tim.leonhard@posteo.de>
2022-12-20 22:50:50 +0100

    optimise and refactor

--- 19_not_enough_minerals/not_enough_minerals.py
+++ 19_not_enough_minerals/not_enough_minerals.py
@@ -1,81 +1,69 @@
 import re
 import time
 
-from functools import cache
-
 begin = time.time()
 
 ###
 
-@cache
-def heuristic(time_left: int, geode_robot_count: int) -> int:
-	return ((time_left-1)**2 + (time_left-1)) // 2  + time_left * geode_robot_count
-
-@cache
-def dfs(time_left: int, stock: tuple, robots: tuple, blueprint: tuple, upper: tuple) -> int:
+def dfs(state: tuple, cache: dict, blp: tuple, upper: tuple, next_bot_type: int) -> int:
 	global best
 
+	if state in cache:
+		return cache[state]
+
+	time_left, stock, robots = state
+
 	if time_left == 0:
+		best = max(best, stock[-1])
 		return stock[-1]
 
-	if stock[-1] + heuristic(time_left, robots[-1]) < best:
+	if stock[-1] + ((time_left-1)**2 + (time_left-1)) // 2  + time_left * robots[-1] < best:
 		return -1
+
+	if next_bot_type < 0:
+		result = max(dfs(state, cache, blp, upper, idx) for idx, _ in enumerate(blp))
+		cache[state] = result
+		return result
+
+	if next_bot_type < 3:
+		enough_of_robot_type = robots[next_bot_type] >= upper[next_bot_type]
+		enough_of_ressource = robots[next_bot_type] * time_left + stock[next_bot_type] >= time_left * upper[next_bot_type]
+		if enough_of_robot_type or enough_of_ressource:
+			return -1
 
 	production = tuple(n + bot for n, bot in zip(stock, robots))
 
-	save = False
-	child_results = []
-	for idx, costs in enumerate(blueprint):
-		if idx < 3 and robots[idx] >= upper[idx]:
-			continue
-		if idx < 3 and robots[idx] * time_left + stock[idx] >= time_left * upper[idx]:
-			continue
-		if any(cost > n for cost, n in zip(costs, stock)):
-			save = True
-			continue
-		new_robots = tuple(bot + (idx==jdx) for jdx, bot in enumerate(robots))
-		new_stock = tuple(n - cost for n, cost in zip(production, costs))
-		child_results.append(dfs(time_left-1, new_stock, new_robots, blueprint, upper))
+	if any(cost > n for cost, n in zip(blp[next_bot_type], stock)):
+		new_state = (time_left-1, production, robots)
+		return dfs(new_state, cache, blp, upper, next_bot_type)
 
-	if save:
-		child_results.append(dfs(time_left-1, production, robots, blueprint, upper))
-
-	result = max(child_results)
-	best = max(best, result)
-	return result
+	new_stock = tuple(n - cost for n, cost in zip(production, blp[next_bot_type]))
+	new_robots = tuple(bot + (next_bot_type==idx) for idx, bot in enumerate(robots))
+	new_state = (time_left-1, new_stock, new_robots)
+	return dfs(new_state, cache, blp, upper, -1)
 
 
 with open("input.txt") as file:
 	blocks = [re.findall(r"\d+", line) for line in file]
 
-blueprints, upper_bounds = [], []
-for numbers in blocks:
-	blp = (
+p1_acc, p2_acc = 0, 1
+for idx, numbers in enumerate(blocks):
+	blueprint = (
 		(int(numbers[1]),0,0,0),
 		(int(numbers[2]),0,0,0),
 		(int(numbers[3]),int(numbers[4]),0,0),
 		(int(numbers[5]),0,int(numbers[6]),0),
 	)
-	blueprints.append(blp)
-	upper_bounds.append(tuple(max(b[i] for b in blp) for i in range(3)))
+	upper_bounds = tuple(max(b[i] for b in blueprint) for i in range(3))
 
-best = 0
-quality_levels = []
-for i, tup in enumerate(zip(blueprints, upper_bounds)):
-	open_geodes = dfs(24, (0,0,0,0), (1,0,0,0), *tup)
-	quality_levels.append(open_geodes * (i + 1))
-	dfs.cache_clear()
 	best = 0
+	init_state = (24, (0,0,0,0), (1,0,0,0))
+	p1_acc += dfs(init_state, {}, blueprint, upper_bounds, -1) * (idx + 1)
+	init_state = (32, (0,0,0,0), (1,0,0,0))
+	p2_acc *= dfs(init_state, {}, blueprint, upper_bounds, -1) if idx < 3 else 1
 
-print(f"Part 1: {sum(quality_levels)}")
-
-acc = 1
-for tup in zip(blueprints[:3], upper_bounds):
-	acc *= dfs(32, (0,0,0,0), (1,0,0,0), *tup)
-	dfs.cache_clear()
-	best = 0
-
-print(f"Part 2: {acc}")
+print(f"Part 1: {p1_acc}")
+print(f"Part 2: {p2_acc}")
 
 ###
 
