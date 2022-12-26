af1b8fce44802e7893db012dd22d03218e0ec07d
tim <tim.leonhard@posteo.de>
2022-12-26 09:13:39 +0100

    switch to direct snafu summation approach

--- 25_full_of_hot_air/full_of_hot_air.py
+++ 25_full_of_hot_air/full_of_hot_air.py
@@ -4,43 +4,40 @@ begin = time.time()
 
 ###
 
-def snafu_to_int(snafu_number: str):
-	acc = 0
-	for idx, digit in enumerate(reversed(snafu_number)):
-		match digit:
-			case "=":
-				value = -2
-			case "-":
-				value = -1
-			case _:
-				value = int(digit)
-		acc += value * 5**idx
-	return acc
+SNAFU_DIGITS = {
+	"=": -2,
+	"-": -1,
+	"0": 0,
+	"1": 1,
+	"2": 2
+}
 
-def base_ten_to_base_five(n: int) -> str:
-	base_five = ""
-	while n > 0:
-		remainder = n % 5
-		base_five = str(remainder) + base_five
-		n = n // 5
-	return base_five
 
-def int_to_snafu(n: int):
-	result = []
-	carry = 0
-	for digit in reversed((base_ten_to_base_five(n))):
-		digit = int(digit) + carry
-		carry = int(digit > 2)
-		result.append(digit - 5*carry)
-	return "".join(str(n) for n in reversed(result)).replace("-1", "-").replace("-2", "=")
+def snafu_add(a: list, b: list, carry=0) -> list:
+	if not a and not b:
+		return [carry] if carry else []
+
+	if not a:
+		return snafu_add([carry], b)
+
+	if not b:
+		return snafu_add(a, [carry])
+
+	digit_sum = a[0] + b[0] + carry
+	carry = int(digit_sum > 2) - int(digit_sum < -2)
+	return [digit_sum - 5*carry] + snafu_add(a[1:], b[1:], carry)
+
+def snafu_sum(numbers: list) -> str:
+	acc = [0]
+	for snafu in numbers:
+		acc = snafu_add(acc, [SNAFU_DIGITS[char] for char in reversed(snafu)])
+	return "".join(str(n) for n in reversed(acc)).replace("-1", "-").replace("-2", "=")
 
 
 with open("input.txt") as file:
 	snafu_numbers = [line.strip() for line in file]
 
-as_int = [snafu_to_int(snafu) for snafu in snafu_numbers]
-
-print(f"Part 1: {int_to_snafu(sum(as_int))}")
+print(f"Part 1: {snafu_sum(snafu_numbers)}")
 
 ###
 
